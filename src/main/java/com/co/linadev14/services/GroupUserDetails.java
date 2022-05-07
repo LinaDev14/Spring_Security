84f02e7f85060eb8a9fb62dcb65c02b5769b764b
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-05-07 15:45:49 -0500

    se crea configuracion de spring security

---
+++ src/main/java/com/co/linadev14/services/GroupUserDetails.java
@@ -0,0 +1,55 @@
+package com.co.linadev14.services;
+
+import com.co.linadev14.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class GroupUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+    private boolean isActive;
+    private List<GrantedAuthority> authorities;
+
+    public GroupUserDetails(User user){
+        this.username = user.getUserName();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
--- src/main/java/com/co/linadev14/services/GroupUserDetailsService.java
+++ src/main/java/com/co/linadev14/services/GroupUserDetailsService.java
@@ -1,10 +1,13 @@
 package com.co.linadev14.services;
 
+import com.co.linadev14.entity.User;
 import com.co.linadev14.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class GroupUserDetailsService implements UserDetailsService {
@@ -13,6 +16,8 @@ public class GroupUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(username);
+
         return null;
     }
 }
